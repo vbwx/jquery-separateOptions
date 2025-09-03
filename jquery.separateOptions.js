3630e6baea091a894dc8ff2877c98d31f13864d3
vbwx <vbwx01@gmail.com>
2025-09-03 21:15:32 +0200

    Update copyright info

--- jquery.separateOptions.js
+++ jquery.separateOptions.js
@@ -1,5 +1,5 @@
 // jQuery separateOptions plugin
-// Copyright (C) 2013-2017 Bernhard Waldbrunner
+// Copyright (C) 2013-2017 vbwx
 /*
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -44,3 +44,4 @@
 		});
 	};
 })(jQuery);
+
